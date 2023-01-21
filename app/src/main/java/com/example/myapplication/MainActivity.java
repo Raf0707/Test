0fbad86a118d485f5070fc1a4ed8c9c170a7fd40
raf_0707 <Raf_0707@mail.ru>
2023-01-21 22:36:47 +0300

    Test

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.example.myapplication.databinding.ActivityMainBinding;
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        super.onCreate(savedInstanceState);
+        setContentView(binding.getRoot());
+    }
+}
